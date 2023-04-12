b4adbcd34843f93fb62488f28c5294406e724e51
jjiny0716 <jjiny3773@naver.com>
2023-04-12 18:52:35 +0900

    feat: Logo 컴포넌트 구현

---
+++ src/components/Logo/Logo.styles.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const LogoLayout = styled.div``;
+
+export const LogoParagraph = styled.p`
+  font-size: 36px;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: #000;
+`;
