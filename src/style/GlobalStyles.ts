19d1f799952c198e7e5cb5d9edd1bb622bd0d1da
jjiny0716 <jjiny3773@naver.com>
2023-04-12 18:58:59 +0900

    feat: GlobalStyles 수정 - 모든 텍스트 color 초기화 - a 태그 스타일링 초기화

--- src/style/GlobalStyles.ts
+++ src/style/GlobalStyles.ts
@@ -10,6 +10,11 @@ const GlobalStyles = createGlobalStyle`
   }
   body {
     font-family: 'Noto Sans KR', sans-serif;
+    color: ${(props) => props.theme.colors.black};
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
   }
 `;
 
