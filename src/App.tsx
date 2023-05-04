ceab81933b33b9839fa92cbebc54b604ae492b97
JS <kjs9382@naver.com>
2023-05-05 00:37:34 +0900

    Revert "Checkbox 컴포넌트 구현시도"

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { ThemeProvider } from "styled-components";
 
 import GlobalStyles from "@/style/GlobalStyles";
 import { theme } from "@/style/theme";
+import MainHeader from "./components/MainHeader/MainHeader.component";
+import Dropdown from "./components/dropdown/Dropdown.component";
+import { BasicCheckbox } from "./components/Checkbox/Checkbox.style";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div></div>
+      <div>
+        {/* <BasicCheckbox></BasicCheckbox> */}
+        <Dropdown text="test"></Dropdown>
+      </div>
       <GlobalStyles />
     </ThemeProvider>
   );
