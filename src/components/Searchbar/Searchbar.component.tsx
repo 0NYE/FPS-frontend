eeb7b47b5f20b67067a266621bca8802696fb690
JS <kjs9382@naver.com>
2023-05-06 23:37:45 +0900

    Searchbar 컴포넌트 구현 시도

---
+++ src/components/Searchbar/Searchbar.component.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { SearchWrapper, SingleSearch, 
+        Glasses, GlassesCircle, GlassesLine,
+} from "@/components/Searchbar/Searchbar.styles";
+
+const Search = ()=>{
+    return (
+        <>
+          <SearchWrapper>
+            <SingleSearch />
+            <Glasses>
+                <GlassesCircle/>
+                <GlassesLine />
+            </Glasses>
+          </SearchWrapper>
+        </>
+        );
+}
+
+export default Search;
---
+++ src/components/Searchbar/Searchbar.styles.tsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const SearchWrapper = styled.div`
+  float: left;
+  position: relative;
+`;
+
+export const SingleSearch = styled.input.attrs({
+  placeholder: "검색"
+})`
+  width: 94.5px;
+  height: 28px;
+  padding: 5px;
+  border: none;
+  border-radius: 4px;
+  background: ${(props)=>props.theme.colors.grey4};
+  box-sizing: border-box;
+  font-size: 12px;
+  outline: none;
+  position: relative;
+  color: ${(props)=>props.theme.colors.black};
+  &::placeholder {
+    font-size: 12px;
+    color: ${(props)=>props.theme.colors.grey1};
+  }
+`;
+
+export const Glasses= styled.span`
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    width: 16px;
+    height: 16px;
+    margin: auto;
+    left:80%;
+    bottom:10%;
+`;
+
+export const GlassesCircle= styled.span`
+    width: 9px;
+    height: 9px;
+    border: 2px solid ${(props)=>props.theme.colors.grey1};
+    border-radius: 50%;
+`;
+
+export const GlassesLine= styled.span`
+    width: 6px;
+    border: 1px solid ${(props)=>props.theme.colors.grey1};
+    border-radius: 1px;
+    transform: translate3d(6px, 0.5px, 0) rotate(45deg);
+    // background-color: ${(props)=>props.theme.colors.black};
+`;
