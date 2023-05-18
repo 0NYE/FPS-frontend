daa720c7f41848832de4f455502e84e908d56916
jjiny0716 <jjiny3773@naver.com>
2023-05-18 21:22:16 +0900

    refactor: ProblemBlock tsx로 전환 및 구조 변경

--- src/components/ProblemBlock/ProblemBlock.styles.tsx
+++ src/components/ProblemBlock/ProblemBlock.styles.tsx
@@ -7,11 +7,18 @@ export const ProblemBlockLayout = styled.div`
   background-color: ${(props) => props.theme.colors.grey4};
 `;
 
-export const ProblemWrap = styled.div`
-  display: inline-block;
+export const ProblemBlockDataBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 `;
 
-export const ProblemTitle = styled.div`
+export const ProblemBlockHeaderBox = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+export const ProblemBlockTitle = styled.h3`
   height: 16px;
   padding: 0px 3px;
   font-weight: bold;
@@ -21,17 +28,19 @@ export const ProblemTitle = styled.div`
   }
 `;
 
-export const ProblemMetaData = styled.div `
+export const ProblemBlockMetaDataBox = styled.div`
   height: 14px;
   font-size: 12px;
   margin-bottom: 12px;
 `;
 
-export const TagData = styled.div`
-  height: 3px;
-`;
+export const ProblemBlockMetaDataSpan = styled.span``;
 
-export const TagBox = styled.div`
+export const ProblemBlockTagBox = styled.div``;
+
+export const ProblemBlockTagList = styled.ul``;
+
+export const TagSpan = styled.span`
   display: inline-block;
   font-size: 12px;
   margin-left: 10px;
@@ -40,7 +49,7 @@ export const TagBox = styled.div`
   background-color: ${(props) => props.theme.colors.grey3};
 `;
 
-export const LanguageBox = styled.div`
+export const ProblemBlockLanguageIconList = styled.ul`
   display: inline-block;
   justify-content: center;
   align-items: center;
