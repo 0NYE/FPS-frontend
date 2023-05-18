fa830e6323a303a9c1ad803ca58c1f684999d60b
jjiny0716 <jjiny3773@naver.com>
2023-05-18 15:32:59 +0900

    feat: LoginModal 구현 - 로그인, 회원가입 탭 전환

--- src/components/LoginModal/LoginModal.component.tsx
+++ src/components/LoginModal/LoginModal.component.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import ReactModal from "react-modal";
 
+import LoginForm from "@/components/LoginForm/LoginForm.component";
 import { modalCustomStyles } from "@/components/LoginModal/LoginModal.styles";
 import Tab from "@/components/LoginModal/Tab/Tab.component";
+import SignUpForm from "@/components/SignUpForm/SignUpForm.component";
 
 interface LoginModalProps {
   isOpen: boolean;
@@ -11,8 +13,14 @@ interface LoginModalProps {
 const modes = ["로그인", "회원가입"] as const;
 type Mode = (typeof modes)[number];
 
+const ModeFormMap: { [K in Mode]: () => JSX.Element } = {
+  로그인: LoginForm,
+  회원가입: SignUpForm,
+};
+
 const LoginModal = ({ isOpen }: LoginModalProps) => {
   const [currentMode, setCurrentMode] = useState<Mode>(modes[0]);
+  const SelectedForm = ModeFormMap[currentMode];
 
   const tabOnChangeHandler = (selectedItem: string) => {
     setCurrentMode(selectedItem as Mode);
@@ -21,6 +29,7 @@ const LoginModal = ({ isOpen }: LoginModalProps) => {
   return (
     <ReactModal isOpen={isOpen} style={modalCustomStyles}>
       <Tab items={modes as unknown as string[]} onChange={tabOnChangeHandler} />
+      <SelectedForm />
     </ReactModal>
   );
 };
