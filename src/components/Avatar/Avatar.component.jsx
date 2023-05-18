13f9d365b03d8c99d65c4cf4c1b3080b7a8c9e60
jjiny0716 <jjiny3773@naver.com>
2023-05-18 17:57:02 +0900

    style: Avatar 파일 코드 포맷팅

--- src/components/Avatar/Avatar.component.jsx
+++ src/components/Avatar/Avatar.component.jsx
@@ -1,32 +1,31 @@
 import React, { useState } from "react";
 
-import { ProfileIMG, Wrapper} from "./Avatar.styles";
+import { ProfileIMG, Wrapper } from "./Avatar.styles";
 
 const sizes = {
   small: {
-    radius: 24
+    radius: 24,
   },
   medium: {
-    radius: 48
+    radius: 48,
   },
   large: {
-    radius: 100
+    radius: 100,
   },
 };
 
-const Avatar = ({size="large",src}) => {
-  const [Image, setImage] = useState(src)
-  const radius =sizes[size].radius;
+const Avatar = ({ size = "large", src }) => {
+  const [Image, setImage] = useState(src);
+  const radius = sizes[size].radius;
 
-  
   return (
     <Wrapper>
-      <ProfileIMG src={Image} 
-      alt="profile" 
-      width={radius}
-      height={radius}
-      >
-      </ProfileIMG>
+      <ProfileIMG
+        src={Image}
+        alt="profile"
+        width={radius}
+        height={radius}
+      ></ProfileIMG>
     </Wrapper>
   );
 };
--- src/components/Avatar/Avatar.styles.jsx
+++ src/components/Avatar/Avatar.styles.jsx
@@ -1,13 +1,10 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
-`;
+export const Wrapper = styled.div``;
 
-export const ProfileIMG= styled.img`
-  width: ${props=>props.width}px;
-  height: ${props=>props.height}px;
+export const ProfileIMG = styled.img`
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
   border-radius: 50%;
   object-fit: cover;
 `;
-
-
