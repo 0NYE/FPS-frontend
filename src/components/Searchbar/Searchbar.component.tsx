d3c920d52e148e68e5df13f61f901d0213304b6d
jjiny0716 <jjiny3773@naver.com>
2023-05-18 17:19:10 +0900

    feat: SearchBar 컴포넌트 스타일 변경 - 돋보기 svg를 왼쪽으로 변경

--- src/components/Searchbar/Searchbar.component.tsx
+++ src/components/Searchbar/Searchbar.component.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 
+import { ReactComponent as Glass } from "@/assets/images/glass.svg";
 import {
   SearchBarLayout,
   SearchBarInput,
+  SvgContainer,
 } from "@/components/Searchbar/Searchbar.styles";
 
 const Search = () => {
   return (
     <SearchBarLayout>
       <SearchBarInput />
+      <SvgContainer>
+        <Glass />
+      </SvgContainer>
     </SearchBarLayout>
   );
 };
--- src/components/Searchbar/Searchbar.styles.tsx
+++ src/components/Searchbar/Searchbar.styles.tsx
@@ -1,26 +1,35 @@
 import styled from "styled-components";
 
 export const SearchBarLayout = styled.div`
-  float: left;
   position: relative;
 `;
 
 export const SearchBarInput = styled.input.attrs({
   placeholder: "검색",
 })`
-  width: 94.5px;
-  height: 28px;
-  padding: 5px;
+  width: 100px;
+  padding: 8px 12px 8px 30px;
   border: none;
   border-radius: 4px;
-  background: ${(props) => props.theme.colors.grey4};
-  box-sizing: border-box;
-  font-size: 12px;
   outline: none;
-  position: relative;
+  background: ${(props) => props.theme.colors.grey4};
   color: ${(props) => props.theme.colors.black};
+  font-size: 14px;
+
   &::placeholder {
-    font-size: 12px;
+    font-size: 13px;
     color: ${(props) => props.theme.colors.grey1};
   }
+`;
+
+export const SvgContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 32px;
+  height: 100%;
+  transform: scale(0.85);
 `;
