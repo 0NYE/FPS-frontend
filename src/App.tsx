d0d432194e3aaacb138eb11147a2b4f42340e6c4
jjiny0716 <jjiny3773@naver.com>
2023-05-18 17:55:08 +0900

    fix: App.jsx를 App.tsx로 변경 및 코드 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import { ThemeProvider } from "styled-components";
 
-import Avatar from "./components/Avatar/Avatar.component";
-
 import GlobalStyles from "@/style/GlobalStyles";
 import { theme } from "@/style/theme";
-
-
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div>
-        <Avatar src={"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"}></Avatar>
-      </div>
+      <div></div>
       <GlobalStyles />
     </ThemeProvider>
   );
