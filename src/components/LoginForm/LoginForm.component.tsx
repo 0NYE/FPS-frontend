1db5af2f4d22f27ffa73ee1625b6fb4d0dc63812
jjiny0716 <jjiny3773@naver.com>
2023-05-19 00:04:14 +0900

    feat: LoginForm 로직 구현 - 로그인

--- src/components/LoginForm/LoginForm.component.tsx
+++ src/components/LoginForm/LoginForm.component.tsx
@@ -3,13 +3,41 @@ import React from "react";
 import Button from "@/components/Button/Button.component";
 import LabeledInput from "@/components/LabeledInput/LabeledInput.component";
 import { LoginFormLayout } from "@/components/LoginForm/LoginForm.style";
+import { domain } from "@/constants/api";
+import { useInput } from "@/hooks/useInput";
 
 const LoginForm = () => {
+  const idInput = useInput("");
+  const passwordInput = useInput("");
+
+  const buttonClickHandler: React.MouseEventHandler<HTMLButtonElement> = () => {
+    fetch(`${domain}/auth/login`, {
+      method: "POST",
+      body: JSON.stringify({
+        id: idInput.value,
+        password: passwordInput.value,
+      }),
+    })
+      .then((response) => {
+        alert("로그인에 성공했습니다.");
+        console.log("login success!");
+      })
+      .catch((err) => {
+        alert("로그인에 실패했습니다.");
+        console.error(err);
+      });
+  };
+
   return (
     <LoginFormLayout>
-      <LabeledInput label="아이디" id="id" type="email" />
-      <LabeledInput label="비밀번호" id="password" type="password" />
-      <Button variant="black" size="large">
+      <LabeledInput label="아이디" id="id" type="email" {...idInput} />
+      <LabeledInput
+        label="비밀번호"
+        id="password"
+        type="password"
+        {...passwordInput}
+      />
+      <Button variant="black" size="large" onClick={buttonClickHandler}>
         로그인
       </Button>
     </LoginFormLayout>
