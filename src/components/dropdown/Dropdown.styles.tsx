11a218950c4d5f13717ab9e6c6b22f4cc0c6626e
Yu Jin <62554964+jjiny0716@users.noreply.github.com>
2023-05-22 16:38:57 +0900

    fix: CategoryMenuBox 오류 수정

--- src/components/dropdown/Dropdown.styles.tsx
+++ src/components/dropdown/Dropdown.styles.tsx
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const DropDownBoxWrap = styled.div`
   position: absolute;
-  top:26px;
-  left:0%;
+  top: 26px;
+  left: 0%;
   display: inline-block;
 `;
 
@@ -30,28 +30,28 @@ export const ListItem = styled.li`
   }
 `;
 
-export const ChevronDown =styled.span`
-    border-style: solid;
-    border-width: 0.25em 0.25em 0 0;
-    display: inline-block;
-    height: 0.5rem;
-    left: 5%;
-    top: -2px;
-    position: relative;
-    transform: rotate(135deg);
-    vertical-align: middle;
-    width: 0.5rem;
+export const ChevronDown = styled.span`
+  border-style: solid;
+  border-width: 0.25em 0.25em 0 0;
+  display: inline-block;
+  height: 0.5rem;
+  left: 5%;
+  top: -2px;
+  position: relative;
+  transform: rotate(135deg);
+  vertical-align: middle;
+  width: 0.5rem;
 `;
 
 export const CategoryMenuBox = styled.button`
-  width: 100%
+  width: 100%;
   height: 100%;
   font-weight: 500;
   font-size: 5px;
   align-items: center;
   display: flex;
   padding: 8px;
-  border:none;
+  border: none;
   border-radius: 4px;
   position: relative;
   background-color: ${(props) => props.theme.colors.grey4};
