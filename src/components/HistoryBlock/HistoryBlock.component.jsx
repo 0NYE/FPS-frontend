2281e56bbbf45831cd3db004895a89c855d9169c
JS <kjs9382@naver.com>
2023-05-23 13:59:52 +0900

    Date객체 반영 및 공백 수정

--- src/components/HistoryBlock/HistoryBlock.component.jsx
+++ src/components/HistoryBlock/HistoryBlock.component.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 
 import { 
-    HistoryLayout,
-    HistoryTime,
-    HistoryTitle 
+  HistoryLayout,
+  HistoryTime,
+  HistoryTitle 
 } from './HistoryBlock.styles';
 
 
 
 const HistoryBlock= ({
-    title,
-    year,
-    mon,
-    day,
-    hour,
-    min
+  title,
 })=>{
-    return(
-        <HistoryLayout>
-            <HistoryTitle>{title}</HistoryTitle>
-            <HistoryTime>{year}-{mon}-{day} {hour}:{min}</HistoryTime>
-        </HistoryLayout>
-    )
+  const currentDate = new Date();
+  const year = currentDate.getFullYear(); 
+  const month = currentDate.getMonth() + 1;
+  const day = currentDate.getDate();
+  const hour = currentDate.getHours();
+  const min = currentDate.getMinutes();
+  return(
+      <HistoryLayout>
+          <HistoryTitle>{title}</HistoryTitle>
+          <HistoryTime>{year}-{month}-{day} {hour}:{min}</HistoryTime>
+      </HistoryLayout>
+  )
 };
 
 export default HistoryBlock;
