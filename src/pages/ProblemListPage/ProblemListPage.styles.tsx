bdc8400cf035a5c75db03fd37d01d0d91da5df43
Yu Jin <62554964+jjiny0716@users.noreply.github.com>
2023-05-25 01:48:05 +0900

    feat: ProblemListPage 구현

--- src/pages/ProblemListPage/ProblemListPage.styles.tsx
+++ src/pages/ProblemListPage/ProblemListPage.styles.tsx
@@ -2,4 +2,21 @@ import styled from "styled-components";
 
 export const ProblemListPageLayout = styled.div``;
 
-export const ProblemListPageMain = styled.main``;
+export const ProblemListPageMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  width: 800px;
+  margin: 32px auto;
+`;
+
+export const ProblemBlockList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+
+  & > li {
+    width: 100%;
+  }
+`;
