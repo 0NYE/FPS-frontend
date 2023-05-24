001174a373ad8f94fddf85bdae88d7c87d82b91c
Yu Jin <62554964+jjiny0716@users.noreply.github.com>
2023-05-24 20:38:48 +0900

    feat: HistoryBlock 컴포넌트 스타일 변경

--- src/components/HistoryBlock/HistoryBlock.styles.tsx
+++ src/components/HistoryBlock/HistoryBlock.styles.tsx
@@ -4,14 +4,13 @@ export const HistoryLayout = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  width: 498px;
-  padding: 16px 16px;
+  padding: 16px;
   background-color: ${(props) => props.theme.colors.grey4};
   color: ${(props) => props.theme.colors.black};
 `;
 
 export const HistoryTitle = styled.div`
-  font-size: 16px;
+  font-size: 18px;
   font-weight: bold;
 `;
 
