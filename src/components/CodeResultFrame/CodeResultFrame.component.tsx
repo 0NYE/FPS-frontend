b1b2ee20fe3fe29b2cbff78853542e0733772f52
Yu Jin <62554964+jjiny0716@users.noreply.github.com>
2023-05-26 04:51:36 +0900

    fix: CodeResultFrame cors 문제 해결

--- src/components/CodeResultFrame/CodeResultFrame.component.tsx
+++ src/components/CodeResultFrame/CodeResultFrame.component.tsx
@@ -26,7 +26,7 @@ const CodeResultFrame = ({
       <iframe
         srcDoc={resultSourceCode}
         title="output"
-        sandbox="allow-scripts"
+        sandbox="allow-same-origin"
         width="100%"
         height="100%"
         {...props}
