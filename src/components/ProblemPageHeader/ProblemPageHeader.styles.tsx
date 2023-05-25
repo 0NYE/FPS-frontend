d704f150378ed7a6156806ba87ac4ece769dc11d
Yu Jin <62554964+jjiny0716@users.noreply.github.com>
2023-05-25 22:46:02 +0900

    feat: ProblemPageHeader 구현

---
+++ src/components/ProblemPageHeader/ProblemPageHeader.styles.tsx
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+
+import styled from "styled-components";
+
+export const ProblemPageHeaderLayout = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+  padding: 0px 16px;
+  border-bottom: 1px solid ${(props) => props.theme.colors.grey3};
+  font-size: 16px;
+`;
+
+export const ProblemPageHeaderNavLink = styled(NavLink)`
+  font-size: 16x;
+  font-weight: 700;
+  color: ${(props) => props.theme.colors.grey1};
+  transition: color 150ms ease;
+
+  &:hover {
+    color: ${(props) => props.theme.colors.black};
+  }
+`;
+
+export const ProblemPageHeaderLoginLink = styled(NavLink)`
+  color: ${(props) => props.theme.colors.grey1};
+`;
