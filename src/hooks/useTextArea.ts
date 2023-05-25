cb3a878b7fd11f40013359a0e509b6b43e14311b
Yu Jin <62554964+jjiny0716@users.noreply.github.com>
2023-05-25 20:39:28 +0900

    fix: useTextArea 훅 이름 수정

--- src/hooks/useTextArea.ts
+++ src/hooks/useTextArea.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export const useInput = (initialValue: string) => {
+export const useTextArea = (initialValue: string) => {
   const [value, setValue] = useState(initialValue);
   const onChange: React.ChangeEventHandler<HTMLTextAreaElement> = (e) => {
     setValue(e.target.value);
