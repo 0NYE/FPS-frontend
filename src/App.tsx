f985a687e929a4719bf0602836afc1a73898ba45
Yu Jin <62554964+jjiny0716@users.noreply.github.com>
2023-05-25 21:34:12 +0900

    feat: CreateProblemPage 구현 및 route 연결

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import { ThemeProvider } from "styled-components";
 
+import CreateProblemPage from "@/pages/CreateProblemPage/CreateProblemPage.component";
 import ProblemListPage from "@/pages/ProblemListPage/ProblemListPage.component";
 import GlobalStyles from "@/style/GlobalStyles";
 import { theme } from "@/style/theme";
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route index element={<ProblemListPage />} />
         <Route path="/problems" element={<ProblemListPage />} />
+        <Route path="/create-problem" element={<CreateProblemPage />} />
       </Routes>
       <GlobalStyles />
     </ThemeProvider>
