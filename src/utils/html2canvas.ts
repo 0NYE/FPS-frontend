ecbd86b50f15454635ff12f52203135efb04431e
Yu Jin <62554964+jjiny0716@users.noreply.github.com>
2023-05-26 04:56:52 +0900

    feat: iframeToPNG 유틸 함수 구현

--- src/utils/html2canvas.ts
+++ src/utils/html2canvas.ts
@@ -1,7 +1,18 @@
 import html2canvas from "html2canvas";
 
-export const elementToPNG = async (element: HTMLElement, fileName: string) => {
-  const canvas = await html2canvas(element);
+export const iframeToPNG = async (
+  element: HTMLIFrameElement,
+  fileName: string
+) => {
+  const target = element.contentWindow?.document.querySelector(
+    "body"
+  ) as HTMLBodyElement;
+  const canvas = await html2canvas(target, {
+    useCORS: true,
+    allowTaint: true,
+    foreignObjectRendering: true,
+  });
+
   const imageUrl = canvas.toDataURL("image/png");
   const blob = await fetch(imageUrl).then((res) => res.blob());
   return new File([blob], `${fileName}.png`, {
