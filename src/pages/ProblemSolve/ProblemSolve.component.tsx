bc16d61356a68280cb1b43ca93559cd5cdc7d04a
Yu Jin <62554964+jjiny0716@users.noreply.github.com>
2023-05-26 02:37:42 +0900

    feat: ProblemSolve UI 구현

--- src/pages/ProblemSolve/ProblemSolve.component.tsx
+++ src/pages/ProblemSolve/ProblemSolve.component.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
+import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
+
+import Button from "@/components/Button/Button.component";
 import CodeResultFrame from "@/components/CodeResultFrame/CodeResultFrame.component";
+import ProgressBar from "@/components/ProgressBar/ProgressBar.components";
 import TwoVerticalSection from "@/components/TwoVerticalSection/TwoVerticalSection.component";
 import { domain } from "@/constants/api";
 import { useFetch } from "@/hooks/useFetch";
@@ -11,6 +15,13 @@ import {
   ProblemDescription,
   ProblemRenderBox,
   ProblemTipParagraph,
+  SolveSectionLayout,
+  EditorControlBox,
+  EditorBox,
+  UserCodeRenderBoxWrapper,
+  UserCodeRenderBox,
+  SubmissionControlBox,
+  ProgressBarBox,
 } from "@/pages/ProblemSolve/ProblemSolve.styles";
 import { ProblemDetailInformation } from "@/types/problem";
 
@@ -19,7 +30,7 @@ const ProblemSolve = () => {
   const { data: problem } = useFetch<ProblemDetailInformation>(
     `http://${domain}/problems/${problem_id}`
   );
-  console.log(problem);
+
   return (
     <>
       {problem ? (
@@ -30,15 +41,41 @@ const ProblemSolve = () => {
               <ProblemDescription>{problem.description}</ProblemDescription>
               <ProblemRenderBox>
                 <CodeResultFrame
-                  html={problem.htmlCode}
-                  css={problem.cssCode}
-                  js={problem.jsCode}
+                  html={problem.HTML_code}
+                  css={problem.CSS_code}
+                  js={problem.JS_code}
                 />
               </ProblemRenderBox>
               <ProblemTipParagraph>
                 잘 모르겠다면 브라우저 개발자 도구를 활용해보세요!
               </ProblemTipParagraph>
             </ProblemSectionLayout>
+          }
+          rightChildren={
+            <SolveSectionLayout>
+              <EditorControlBox></EditorControlBox>
+              <EditorBox>
+                <Editor
+                  defaultLanguage="html"
+                  loading="에디터를 불러오는 중입니다..."
+                  options={{
+                    minimap: { enabled: false },
+                    scrollbar: { vertical: "auto", horizontal: "auto" },
+                  }}
+                />
+              </EditorBox>
+              <UserCodeRenderBoxWrapper>
+                <UserCodeRenderBox>
+                  <CodeResultFrame />
+                </UserCodeRenderBox>
+              </UserCodeRenderBoxWrapper>
+              <SubmissionControlBox>
+                <ProgressBarBox>
+                  <ProgressBar value={50} label="유사도: 50%" />
+                </ProgressBarBox>
+                <Button variant="green">제출</Button>
+              </SubmissionControlBox>
+            </SolveSectionLayout>
           }
         />
       ) : (
--- src/pages/ProblemSolve/ProblemSolve.styles.tsx
+++ src/pages/ProblemSolve/ProblemSolve.styles.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 export const ProblemSectionLayout = styled.div`
-  height: 100%;
   display: flex;
   flex-direction: column;
-  padding: 16px;
+  height: 100%;
+  padding: 16px 16px 0 16px;
 `;
 
 export const ProblemTitle = styled.h2`
@@ -20,11 +20,62 @@ export const ProblemDescription = styled.p`
 
 export const ProblemRenderBox = styled.div`
   min-height: 500px;
+  border: 1px solid ${(props) => props.theme.colors.grey3};
 `;
 
 export const ProblemTipParagraph = styled.p`
-  min-height: 24px;
+  min-height: 48px;
+  line-height: 48px;
   font-size: 12px;
   color: ${(props) => props.theme.colors.grey1};
   text-align: right;
+`;
+
+export const SolveSectionLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: stretch;
+  position: relative;
+  height: 100%;
+`;
+
+export const EditorControlBox = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: translateY(-100%);
+  width: 100%;
+  height: 32px;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  background-color: ${(props) => props.theme.colors.white};
+`;
+
+export const EditorBox = styled.div`
+  flex-grow: 1;
+  margin-bottom: 8px;
+  padding-top: 8px;
+  border-top: 1px solid ${(props) => props.theme.colors.grey3};
+`;
+
+export const UserCodeRenderBoxWrapper = styled.div`
+  padding: 0px 16px;
+`;
+
+export const UserCodeRenderBox = styled.div`
+  min-height: 500px;
+  border: 1px solid ${(props) => props.theme.colors.grey3};
+`;
+
+export const SubmissionControlBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 48px;
+  gap: 32px;
+  padding: 0px 16px;
+`;
+
+export const ProgressBarBox = styled.div`
+  width: 75%;
 `;
