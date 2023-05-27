7f5610f0c6a7bc7f2cdae09797a360c8466f7c64
Yu Jin <62554964+jjiny0716@users.noreply.github.com>
2023-05-27 20:50:30 +0900

    feat: 태그 스타일 변경

--- src/components/Tag/Tag.component.tsx
+++ src/components/Tag/Tag.component.tsx
@@ -1,18 +1,21 @@
 import { TagLayout, TagDeleteButton } from "@/components/Tag/Tag.styles";
 
+export type TagSize = "small" | "medium";
+
 interface TagProps {
   children: string;
+  size?: TagSize;
   removable?: boolean;
   onDelete?: (target: string) => void;
 }
 
-const Tag = ({ children, removable, onDelete }: TagProps) => {
+const Tag = ({ children, size = "small", removable, onDelete }: TagProps) => {
   const buttonClickHandler = () => {
     onDelete && onDelete(children);
   };
 
   return (
-    <TagLayout>
+    <TagLayout size={size}>
       {children}
       {removable && (
         <TagDeleteButton onClick={buttonClickHandler}>&#10005;</TagDeleteButton>
--- src/components/Tag/Tag.styles.tsx
+++ src/components/Tag/Tag.styles.tsx
@@ -1,9 +1,28 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const TagLayout = styled.div`
-  padding: 2px 6px;
-  border-radius: 4px;
-  background-color: ${(props) => props.theme.colors.grey3};
+import { TagSize } from "@/components/Tag/Tag.component";
+
+export const TagLayout = styled.div<{ size: TagSize }>`
+  border-radius: 999px;
+  background-color: ${(props) => props.theme.colors.grey3 + "88"};
+  transition: background-color 200ms ease;
+  cursor: pointer;
+
+  ${({ size }) =>
+    ({
+      small: css`
+        padding: 4px 8px;
+        font-size: 13px;
+      `,
+      medium: css`
+        padding: 6px 12px;
+        font-size: 16px;
+      `,
+    }[size])}
+
+  &:hover {
+    background-color: ${(props) => props.theme.colors.grey3 + "FF"};
+  }
 `;
 
 export const TagDeleteButton = styled.button`
