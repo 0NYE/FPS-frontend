1c7de695aff98a8f07a89515bf1f665c69585f1f
Yu Jin <62554964+jjiny0716@users.noreply.github.com>
2023-05-27 22:05:35 +0900

    feat: TagInput 컴포넌트 구현

---
+++ src/components/Tag/TagInput.styled.tsx
@@ -0,0 +1,23 @@
+import styled, { css } from "styled-components";
+
+import { TagSize } from "@/components/Tag/Tag.component";
+
+export const TagInputLayout = styled.div<{ size: TagSize }>`
+  position: relative;
+  border-radius: 999px;
+  background-color: ${(props) => props.theme.colors.grey3 + "88"};
+
+  ${({ size }) =>
+    ({
+      small: css`
+        min-width: 21px;
+        padding: 4px 8px;
+        font-size: 13px;
+      `,
+      medium: css`
+        min-width: 28px;
+        padding: 6px 12px;
+        font-size: 16px;
+      `,
+    }[size])}
+`;
