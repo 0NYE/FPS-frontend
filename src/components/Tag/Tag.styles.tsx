17aa9f32aafb6c403260f160419b42fef9d01600
Yu Jin <62554964+jjiny0716@users.noreply.github.com>
2023-05-27 22:32:00 +0900

    feat: Tag 스타일 변경

--- src/components/Tag/Tag.styles.tsx
+++ src/components/Tag/Tag.styles.tsx
@@ -4,6 +4,7 @@ import { TagSize } from "@/components/Tag/Tag.component";
 
 export const TagLayout = styled.div<{ size: TagSize }>`
   display: flex;
+  align-items: center;
   border-radius: 999px;
   background-color: ${(props) => props.theme.colors.grey3 + "88"};
   transition: background-color 200ms ease;
@@ -27,9 +28,13 @@ export const TagLayout = styled.div<{ size: TagSize }>`
 `;
 
 export const TagDeleteButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-height: 16px;
+  padding: 0 6px;
   margin-right: -6px;
   background-color: transparent;
-  font-size: 10px;
   color: ${(props) => props.theme.colors.black};
   font-weight: 700;
   border: none;
