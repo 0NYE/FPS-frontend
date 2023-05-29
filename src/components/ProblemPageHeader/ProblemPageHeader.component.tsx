95bf0121d198d4f7a812ed2b4d85610f2ac8ad22
Yu Jin <62554964+jjiny0716@users.noreply.github.com>
2023-05-29 13:45:16 +0900

    feat: 문제 앞, 뒤로가기 버튼 추가

--- src/components/ProblemPageHeader/ProblemPageHeader.component.tsx
+++ src/components/ProblemPageHeader/ProblemPageHeader.component.tsx
@@ -1,5 +1,10 @@
+import { useParams } from "react-router-dom";
+
 import { useAtom } from "jotai";
 
+import { ReactComponent as ChevronLeft } from "@/assets/images/chevron_left.svg";
+import { ReactComponent as VerticalList } from "@/assets/images/list.svg";
+import { ReactComponent as ChevronRight } from "@/assets/images/RightChevron.svg";
 import { loginModalActiveAtom } from "@/atoms/modal";
 import { userAtom } from "@/atoms/user";
 import Avatar from "@/components/Avatar/Avatar.component";
@@ -7,10 +12,14 @@ import Logo from "@/components/Logo/Logo.component";
 import {
   ProblemPageHeaderLayout,
   ProblemPageHeaderLoginButton,
+  ProblemPageHeaderControls,
+  ProblemPageHeaderNextLinkBox,
   ProblemPageHeaderNavLink,
+  ProblemPageHeaderNextLink,
 } from "@/components/ProblemPageHeader/ProblemPageHeader.styles";
 
 const ProblemPageHeader = () => {
+  const { problem_id } = useParams();
   const [loginModalActive, setLoginModalActive] = useAtom(loginModalActiveAtom);
   const [user] = useAtom(userAtom);
 
@@ -21,9 +30,26 @@ const ProblemPageHeader = () => {
   return (
     <ProblemPageHeaderLayout>
       <Logo />
-      <ProblemPageHeaderNavLink to="/problems">
-        문제 리스트
-      </ProblemPageHeaderNavLink>
+      <ProblemPageHeaderControls>
+        <ProblemPageHeaderNextLinkBox disabled={problem_id === "1"}>
+          <ProblemPageHeaderNextLink
+            to={`/problems/${Number(problem_id) - 1}/solve`}
+          >
+            <ChevronLeft />
+          </ProblemPageHeaderNextLink>
+        </ProblemPageHeaderNextLinkBox>
+        <ProblemPageHeaderNavLink to="/problems">
+          <VerticalList />
+          문제 리스트
+        </ProblemPageHeaderNavLink>
+        <ProblemPageHeaderNextLinkBox disabled={false}>
+          <ProblemPageHeaderNextLink
+            to={`/problems/${Number(problem_id) + 1}/solve`}
+          >
+            <ChevronRight />
+          </ProblemPageHeaderNextLink>
+        </ProblemPageHeaderNextLinkBox>
+      </ProblemPageHeaderControls>
       {!user ? (
         <ProblemPageHeaderLoginButton onClick={loginButtonClickHandler}>
           로그인
--- src/components/ProblemPageHeader/ProblemPageHeader.styles.tsx
+++ src/components/ProblemPageHeader/ProblemPageHeader.styles.tsx
@@ -24,7 +24,14 @@ export const ProblemPageHeaderLoginButton = styled.button`
   }
 `;
 
+export const ProblemPageHeaderControls = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 export const ProblemPageHeaderNavLink = styled(NavLink)`
+  display: flex;
+  align-items: center;
   font-size: 16x;
   font-weight: 700;
   color: ${(props) => props.theme.colors.grey1};
@@ -32,9 +39,52 @@ export const ProblemPageHeaderNavLink = styled(NavLink)`
 
   &:hover {
     color: ${(props) => props.theme.colors.black};
+
+    & > svg {
+      stroke: ${(props) => props.theme.colors.black};
+    }
+  }
+
+  & > svg {
+    stroke: ${(props) => props.theme.colors.grey1};
+    width: 16px;
+    height: 16px;
+    margin: 1px 2px 0 0;
+    transition: stroke 150ms ease;
   }
 `;
 
 export const ProblemPageHeaderLoginLink = styled(NavLink)`
   color: ${(props) => props.theme.colors.grey1};
+`;
+
+export const ProblemPageHeaderNextLinkBox = styled.div<{ disabled?: boolean }>`
+  opacity: ${(props) => (props.disabled ? 0.5 : 1)};
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
+
+  & > a {
+    pointer-events: ${(props) => (props.disabled ? "none" : "initial")};
+  }
+`;
+
+export const ProblemPageHeaderNextLink = styled(NavLink)`
+  display: flex;
+  margin: 16px;
+  padding: 10px;
+  border-radius: 6px;
+  background-color: #e7e7e786;
+  transition: background-color 180ms ease;
+
+  &:hover {
+    background-color: #e7e7e7ff;
+
+    & > svg {
+      stroke: ${(props) => props.theme.colors.blue};
+    }
+  }
+
+  & > svg {
+    width: 12px;
+    height: 12px;
+  }
 `;
