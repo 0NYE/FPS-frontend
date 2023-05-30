7f5f323c6b74a931e0c0012128ec6e025cb7a26f
Yu Jin <62554964+jjiny0716@users.noreply.github.com>
2023-05-30 23:46:07 +0900

    feat: Avatar shadow 적용

--- src/components/Avatar/Avatar.styles.tsx
+++ src/components/Avatar/Avatar.styles.tsx
@@ -6,5 +6,6 @@ export const ProfileIMG = styled.img`
   width: ${(props) => props.width};
   height: ${(props) => props.height};
   border-radius: 50%;
+  box-shadow: ${(props) => props.theme.shadows.medium};
   object-fit: cover;
 `;
